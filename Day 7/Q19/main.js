d547c63aaa39420f149cb3b5c3be6fdc93e1875a
wal1shah <syedkshahbukhari@gmail.com>
2024-03-11 04:26:37 +0500

    Added changes

---
+++ Day 7/Q19/main.js
@@ -0,0 +1,3 @@
+"use strict";
+let guests = ["Albert Einstien", "Elon Musk", "Mark Zuckerburg"];
+console.log(`I am inviting ${guests.length} people to dinner`);
---
+++ Day 7/Q19/main.ts
@@ -0,0 +1,2 @@
+let guests: string[] = ["Albert Einstien", "Elon Musk", "Mark Zuckerburg"];
+console.log(`I am inviting ${guests.length} people to dinner`);
