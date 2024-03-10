7641bddaed13834aa4457cf737f9911220077a5c
Syed Ali Kazim Shah <162929580+wal1shah@users.noreply.github.com>
2024-03-11 04:32:01 +0500

    Delete Day 7/Q19 directory

--- Day 7/Q19/main.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-let guests = ["Albert Einstien", "Elon Musk", "Mark Zuckerburg"];
-console.log(`I am inviting ${guests.length} people to dinner`);
--- Day 7/Q19/main.ts
+++
@@ -1,2 +0,0 @@
-let guests: string[] = ["Albert Einstien", "Elon Musk", "Mark Zuckerburg"];
-console.log(`I am inviting ${guests.length} people to dinner`);
